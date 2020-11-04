b83fbd12d931a48ac47a07a045102f2c6a4c3294
Moscoso Maximiliano <moscosomaximiliano@gmail.com>
2020-11-03 22:32:19 -0300

    Fixed problems on JS validation form and deleted comments on code

--- TrabajoPracticoObligatorio2/Scripts/popUpInformation.js
+++ TrabajoPracticoObligatorio2/Scripts/popUpInformation.js
@@ -3,17 +3,10 @@ function ShowInformation(item) {
 
     const article = document.getElementById(item);
     
-    console.log(article);
     
     document.getElementById('POPImg').src = article.dataset.img;
-    // document.getElementById('POPImg').src = item.ID;
     document.getElementById('POPName').innerHTML = article.dataset.name;
-    // document.getElementById('POPImg').src = item.Name;
     document.getElementById('POPPrice').innerHTML = "Price: " + article.dataset.price;
-    // document.getElementById('POPImg').src = item.Price;
     document.getElementById('POPDescription').innerHTML = article.dataset.description;
-    // document.getElementById('POPImg').src = item.Description;
-    //
     document.getElementById('POPClose').href = "#" + article.dataset.idName;
-    // document.getElementById('POPClose').href = "#" + item.ID;
 }
